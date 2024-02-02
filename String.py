cd1c9c859d8aee016541bac0b6107e5ccf8ff897
vansh <vanshvagadia1602@gmail.com>
2024-02-03 00:14:11 +0530

    My new python code file

---
+++ String.py
@@ -0,0 +1,20 @@
+# def upper(string):
+#     return string.upper()
+# def is_upper(string):
+#     return string.isupper()
+# def lower(string):
+#     return string.lower()
+# def is_lower(string):
+#     return string.islower()
+# def is_alpha(string):
+#     return string.isalpha()
+# def is_alnum(string):
+#     return string.isalnum()
+# def is_digit(string):
+#     return string.isdigit()
+# def split(string,seperator):
+#     return string.split(seperator)
+
+a=1
+a+=1
+print(a)
